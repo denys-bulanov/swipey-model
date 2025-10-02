f4956e2f44f83bce2ce5b90bd7be8b5150f45ee7
Denys <deniiserbull@gmail.com>
2025-10-02 13:03:26 +0400

    changed link to get models

--- src/api/characterApi.ts
+++ src/api/characterApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const getCharacter = async (slug: string) => {
-  const { data } = await axios.get(`https://swipey.ai/api/v1/characters/public-profile?slug=${slug}`)
+  const { data } = await axios.get(`https://${process.env.NEXT_PUBLIC_URL}/api/v1/characters/public-profile?slug=${slug}`)
   const previewVideo = data.meta?.previews?.find((item: string) => item.includes('video_preview.mp4')) ?? null
   return {
     name: data.name,
