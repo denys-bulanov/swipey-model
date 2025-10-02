0c56f28156f542803fd451e98af1682ee76f11aa
Denys <deniiserbull@gmail.com>
2025-10-02 13:17:26 +0400

    changed link to get models

--- src/api/characterApi.ts
+++ src/api/characterApi.ts
@@ -1,16 +1,27 @@
 import axios from 'axios'
 
 export const getCharacter = async (slug: string) => {
-  const { data } = await axios.get(`https://${process.env.NEXT_PUBLIC_URL}/api/v1/characters/public-profile?slug=${slug}`)
-  const previewVideo = data.meta?.previews?.find((item: string) => item.includes('video_preview.mp4')) ?? null
-  return {
-    name: data.name,
-    age: data.age,
-    data,
-    picture: data.meta?.previews?.[0] ?? null,
-    price: data.amountInUsd ?? null,
-    verified: data.type ?? null,
-    slug: data.slug ?? null,
-    previewVideo: previewVideo,
+  try {
+    const { data } = await axios.get(`https://${process.env.NEXT_PUBLIC_URL}/api/v1/characters/public-profile?slug=${slug}`)
+
+    const previewVideo = data.meta?.previews?.find((item: string) => item.includes('video_preview.mp4')) ?? null
+
+    return {
+      name: data.name,
+      age: data.age,
+      data,
+      picture: data.meta?.previews?.[0] ?? null,
+      price: data.amountInUsd ?? null,
+      verified: data.type ?? null,
+      slug: data.slug ?? null,
+      previewVideo,
+    }
+  } catch (err: any) {
+    if (axios.isAxiosError(err)) {
+      // если бэк прислал message — вытаскиваем
+      const message = err.response?.data?.message || err.message || 'Something went wrong'
+      throw new Error(message)
+    }
+    throw err
   }
 }
--- src/components/LoadingProvider/LoadingProvider.tsx
+++ src/components/LoadingProvider/LoadingProvider.tsx
@@ -1,11 +1,20 @@
 'use client'
 
 import { useAppSelector } from '@/lib/hooks'
+import Header from '@/src/shared/Header/Header'
 
 import FullScreenLoader from '../FullScreenLoader/FullScreenLoader'
 
 function LoadingProvider({ children }: { children: React.ReactNode }) {
-  const loading = useAppSelector(state => state.character.loading)
+  const { loading, error } = useAppSelector(state => state.character)
+
+  if (error) {
+    return (
+      <div className='fixed inset-0 bg-black flex items-center justify-center z-50'>
+        <Header pinkText={error} text='' />
+      </div>
+    )
+  }
 
   return (
     <>
